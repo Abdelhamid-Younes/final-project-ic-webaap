474f4466c6bda4aa38d181742ac3c29edbba17aa
Abdelhamid-younes <younesabdh@yahoo.fr>
2024-11-27 20:48:34 +0000

    update backend.tf file

--- sources/terraform/dev/backend.tf
+++ sources/terraform/dev/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "terrafor-backend-hamid"
     key = "webapp-dev.tfstate"
     region = "us-east-1"
+    shared_credentials_files = ["C:/Users/Administrados/aws_credentials"]
   }
 }
