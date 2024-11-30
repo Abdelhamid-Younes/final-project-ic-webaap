016adaa65ebd3f2b4670c54974a6328ec0312d0e
Abdelhamid-younes <younesabdh@yahoo.fr>
2024-11-30 10:04:24 +0000

    Update terraform files

--- sources/terraform/dev/backend.tf
+++ sources/terraform/dev/backend.tf
@@ -4,8 +4,8 @@ terraform {
     key = "webapp-dev.tfstate"
     region = "us-east-1"
     #shared_credentials_files = ["C:/Users/Administrados/aws_credentials2"]
-    access_key = "XXXXXXXXXXXXXXXXXXXX"
-    secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  
+    access_key = "AKxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"   
 
   }
 }
--- sources/terraform/dev/provider.tf
+++ sources/terraform/dev/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.region
   #shared_credentials_files = ["C:/Users/Administrados/aws_credentials"]
-  access_key = "XXXXXXXXXXXXXXXXXXXXXXXXX"
-  secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  access_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  
 }
 
 variable "region" {
