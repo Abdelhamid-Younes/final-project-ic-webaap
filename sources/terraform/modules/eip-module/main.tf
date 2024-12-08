a71f97c0238e70f052488eebce46d5820f1a627d
Abdelhamid-younes <younesabdh@yahoo.fr>
2024-12-08 21:34:31 +0000

    Update eip terraform module

--- sources/terraform/modules/eip-module/main.tf
+++ sources/terraform/modules/eip-module/main.tf
@@ -2,7 +2,7 @@ resource "aws_eip" "my_eip" {
     domain = "vpc"
 
     provisioner "local-exec" {
-        command = "echo IP: ${aws_eip.my_eip.public_ip} > /var/jenkins_home/workspace/ic-webapp/ec2_IP.txt"
+        command = "echo IP: ${aws_eip.my_eip.public_ip} > /var/jenkins_home/workspace/ic-webapp@2/ec2_IP.txt"
     }
 }
 
